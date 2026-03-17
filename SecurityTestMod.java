75e2d1315dbd53c41238b3ae4522b7429b7e6820
higginsonlennox-prog <higginson.lennox@gmail.com>
2026-03-17 17:20:41 +1100

    Update SecurityTestMod.java

--- SecurityTestMod.java
+++ SecurityTestMod.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example;
+package net/fabricmc/example/
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
